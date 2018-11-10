0eb17395f3dff8779557ca6016628d5238c3a966
Ivan <43045075+IvankoSL@users.noreply.github.com>
2018-11-10 20:16:35 +0200

    changes
    changed the menu and background

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
-menu.onclick = function myFunction() {
- var x = document.getElementById("myTopnav");
-  
-  if (x.className === "topnav") {
-  	x.className += " responsive";
-  } else {
-  	x.className ="topnav";
-  }
-}
+ 
+      document.getElementById('menuOpen').onclick = menuShou;
+      document.getElementById('myTopnav').onclick = menuHide;
+ 
+
+      function menuShou() {
+ 	      document.getElementById('myTopnav').style.left = '1px';
+      }
+
+
+      function menuHide() {
+	      document.getElementById('myTopnav').style.left = '-112px';
+      }
