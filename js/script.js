159936b7661c66ddb1af8429586427c3c6bd60bc
Ivan <43045075+IvankoSL@users.noreply.github.com>
2018-11-10 21:25:26 +0200

    chenge folders

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
  
-      document.getElementById('menuOpen').onclick = menuShou;
-      document.getElementById('myTopnav').onclick = menuHide;
+document.getElementById('menuOpen').onclick = menuShou;
+document.getElementById('myTopnav').onclick = menuHide;
  
 
       function menuShou() {
@@ -11,3 +11,13 @@
       function menuHide() {
 	      document.getElementById('myTopnav').style.left = '-112px';
       }
+
+function slowScroll (id) {
+	var offset = 70;
+	$('html, body').animate ({
+		scrollTop: $(id).offset ().top - offset
+	}, 900);
+	return false;
+}
+
+ new WOW().init();
